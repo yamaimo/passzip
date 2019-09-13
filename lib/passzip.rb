6efa7374ca3a22c3c51be146a94cb895f4ed2280
yamaimo <hello@yamaimo.dev>
2019-09-13 14:30:04 +0900

    Add simplest feature.

--- lib/passzip.rb
+++ lib/passzip.rb
@@ -1,6 +1,82 @@
 require "passzip/version"
 
 module Passzip
-  class Error < StandardError; end
-  # Your code goes here...
+  AvailableChars = [
+#    "0", # o, O
+#    "1", # l, I
+#    "2", # z, Z
+    "3",
+    "4",
+#    "5", # s, S
+#    "6", # b
+    "7",
+    "8",
+    "9",
+    "a",
+#    "b", # 6
+    "c",
+    "d",
+    "e",
+    "f",
+#    "g", # q
+    "h",
+    "i",
+    "j",
+#    "k", # K
+#    "l", # 1, I
+    "m",
+    "n",
+#    "o", # 0, O
+    "p",
+#    "q", # g
+    "r",
+#    "s", # 5, S
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+#    "z", # 2, Z
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+#    "I", # 1, l
+    "J",
+#    "K", # k
+    "L",
+    "M",
+    "N",
+#    "O", # 0, o
+    "P",
+    "Q",
+    "R",
+#    "S", # 5, s
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+#    "Z", # 2, z
+  ]
+
+  def generate_password(length)
+    (1..length).map {AvailableChars.sample}.join
+  end
+
+  def zipfile(filename, password)
+    zipfilename = filename + ".zip"
+    success = system("zip", "-P", password, zipfilename, filename)
+    if success.nil? || !success
+      raise "Failed to execute zip command."
+    end
+  end
+
+  module_function :generate_password, :zipfile
 end
